d1dcceb9ed46cb4a58f8bf51a117a32c1b985f61
Adam <acdoyle630@gmail.com>
2017-05-19 14:22:06 -1000

    dependencies and new port

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const express = require('express');
 const app = express();
 //const db = require('./models');
-const PORT = process.envPORT || 3000;
+const PORT = process.envPORT || 6969;
 
-app.listen(3000, () =>{
+app.listen(6969, () =>{
   console.log(`server listening on port: ${PORT}`);
 });
