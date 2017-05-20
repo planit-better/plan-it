a141097b61f8b97e01877ec39d43f83d49b16404
Adam <acdoyle630@gmail.com>
2017-05-19 19:39:27 -1000

    fixed merge conflicts

--- models/Contractors.js
+++ models/Contractors.js
@@ -4,17 +4,13 @@ const router = express.Router();
 
 module.exports = function(sequelize, DataTypes) {
   var Contractors = sequelize.define("Contractors", {
-<<<<<<< HEAD
-    name: DataTypes.TEXT,
-    cost: DataTypes.DECIMAL
-=======
+
     company_name: DataTypes.STRING,
     cost: DataTypes.DECIMAL,
     contact: DataTypes.INTEGER,
     date_hired: DataTypes.DATEONLY,
     deadline: DataTypes.DATEONLY
 
->>>>>>> acde7913318b42923fa415803e01810363836cfe
   }, {
     classMethods:  {
       associate: function(models) {
