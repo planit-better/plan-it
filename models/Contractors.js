6fc94d922d8576d45c08591c1ce144f9700397ea
Adam <acdoyle630@gmail.com>
2017-05-23 17:06:41 -1000

    added one to one contractor to task relation

--- models/Contractors.js
+++ models/Contractors.js
@@ -14,7 +14,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods:  {
       associate: function(models) {
-        // Contractors.belongsTo(models.User);
+        Contractors.belongsTo(models.Task);
       }
     }
   });
