261c28e7b291fd6014c7e5a7b333a3772746a5b0
Alexander Davis <akonidavis@gmail.com>
2017-05-25 11:47:06 -1000

    event db and models created

--- api/index.js
+++ api/index.js
@@ -8,5 +8,6 @@ Router.use('/Guest', require('./Guest'));
 Router.use('/Menu', require('./Menu'));
 Router.use('/Task', require('./Task'));
 Router.use('/User', require('./User'));
+Router.use('/Event', require('./Event'));
 
 module.exports = Router;
