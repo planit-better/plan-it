9b4f0511cac1708b694c2c8f971616ae21049f72
Adam <acdoyle630@gmail.com>
2017-05-25 16:56:26 -1000

    logout, need to rerender

--- api/text/index.js
+++ api/text/index.js
@@ -6,7 +6,7 @@ const { groupText, getNumber } = require('../../text-reminder/text.js');
 const { Guest } = require('../../models');
 
 text.post('/', (req,res) =>{
-  console.log(req.body)
+  console.log(req.body);
   Guest.all()
   .then((allGuests) => {
    // console.log(allGuests);
---
+++ src/containers/App/eventForm.js
@@ -0,0 +1,83 @@
+/*jshint esversion: 6*/
+
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './styles.css';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom'
+import { logOut } from '../../action';
+
+
+
+class EventForm extends Component {
+
+  constructor(props){
+
+    super(props);
+
+
+  }
+
+  signOut=()=>{
+    fetch('/logout', {
+      method: "GET"
+    }).then(data =>{
+      return(data.json())
+    }).then(response =>{
+      //sign out action
+      this.props.logOut(response)
+      console.log(response)
+    })
+  }
+
+    render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Planit-Better</h2>
+        </div>
+        <div id="navBar">
+        <button onClick={this.signOut}>Change User</button>
+        </div>
+
+
+
+      </div>
+    );
+  }
+
+}
+
+const mapStateToProps = (state) => {
+  return {
+    contractors : state.contractors,
+    equipment : state.equipment,
+    guest : state.guest,
+    menu : state.menu,
+    task : state.task,
+    currentUser : state.authenticate
+  };
+}
+
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    logOut : currentUser => {
+      dispatch(logOut(currentUser))
+    }
+  }
+}
+
+
+
+
+const ConnectedEventApp = connect(
+  mapStateToProps,
+  mapDispatchToProps
+  )(EventForm);
+
+
+
+export default ConnectedEventApp;
+
