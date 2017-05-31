7d27ef4117c1f39ff85bcc0604960ae1db55520b
Dru808 <andrew.tram@yahoo.com>
2017-05-31 09:24:29 -1000

    resolved 148-new-guest-form page is completly setup

--- frontEnd/src/containers/App/newGuestForm.js
+++ frontEnd/src/containers/App/newGuestForm.js
@@ -152,14 +152,14 @@ class newGuestForm extends Component {
           <h2>Planit-Better</h2>
           <h3>{this.props.eventStatus.currentEvent.name}</h3>
           <h3>{this.props.currentUser.username}</h3>
+          <div>
+            <Link to="/"><button className="button is-outlined is-small">Home</button></Link>
+          </div>
         </div>
 
-          <div>
-            <Link to="/"><button className="button formsHome is-outlined is-small">Home</button></Link>
-          </div>
 
         <div>
-            <button className="button formsHome is-outlined is-small" onClick={this.openForm}>New Guest Form1</button>
+            <button className="button bottomButton is-outlined is-small" onClick={this.openForm}>New Guest Form</button>
         </div>
 
         <div className="columns">
@@ -193,7 +193,12 @@ class newGuestForm extends Component {
             <div className="field">
               <p className="control">
                 <label className="label">Email</label>
+              </p>
+              <p className="control has-icons-left">
                 <input className="input" type="text" placeholder="email" value={this.state.email} onChange={this.handleChangeEmail} />
+                <span className="icon is-small is-left">
+                  <i className="fa fa-envelope"></i>
+                </span>
               </p>
             </div>
 
@@ -272,10 +277,10 @@ class newGuestForm extends Component {
             <h2>Planit-Better</h2>
             <h3>{this.props.eventStatus.currentEvent.name}</h3>
             <h3>{this.props.currentUser.username}</h3>
-          </div>
             <div>
               <Link to="/"><button className="button is-outlined is-small">Home</button></Link>
             </div>
+          </div>
 
           <div>
               <button className="button bottomButton is-outlined is-small" onClick={this.openForm}>New Guest Form</button>
