af1a37743b20af84ad034eea233b4a2af684380a
Adam <acdoyle630@gmail.com>
2017-05-31 19:42:54 -1000

    created contractor profile

--- frontEnd/src/components/contractorList.js
+++ frontEnd/src/components/contractorList.js
@@ -75,6 +75,9 @@ const mapDispatchToProps = (dispatch, ownProps) => {
   return {
     loadContractors: contractors =>{
       dispatch(loadContractors(contractors))
+    },
+    loadCurrentContractor : contractor =>{
+      dispatch(loadCurrentContractor(contractor))
     }
   }
 }
