7e443ed62fedeaef6fd0a10390d5451ea7fcc277
Adam <acdoyle630@gmail.com>
2017-06-02 19:24:08 -1000

    included $ on budget

--- frontEnd/src/containers/App/budgetForm.js
+++ frontEnd/src/containers/App/budgetForm.js
@@ -96,27 +96,27 @@ class Budget extends Component {
                   </tr>
                   <tr id="contractorBudget">
                     <td >Contractor</td>
-                    <td>{contractorBudget}</td>
+                    <td>${contractorBudget}</td>
                     <td>{contractorBudget / (contractorBudget + menuBudget + equipmentBudget + taskBudget)}</td>
                   </tr>
                   <tr id="menuBudget">
                     <td>Menu</td>
-                    <td>{menuBudget}</td>
+                    <td>${menuBudget}</td>
                     <td>{menuBudget / (contractorBudget + menuBudget + equipmentBudget + taskBudget)}</td>
                   </tr>
                   <tr id="equipmentBudget">
                     <td >Equipment</td>
-                    <td>{equipmentBudget}</td>
+                    <td>${equipmentBudget}</td>
                     <td>{Number(equipmentBudget / (contractorBudget + menuBudget + equipmentBudget + taskBudget).toFixed(2))}</td>
                   </tr>
                   <tr id="taskBudget">
                     <td >Task</td>
-                    <td>{taskBudget}</td>
+                    <td>${taskBudget}</td>
                     <td>{Number(taskBudget / (contractorBudget + menuBudget + equipmentBudget + taskBudget).toFixed(2))}</td>
                   </tr>
                   <tr id="totalBudget">
                     <td> Total </td>
-                    <td> {equipmentBudget + contractorBudget + menuBudget + taskBudget}</td>
+                    <td> ${equipmentBudget + contractorBudget + menuBudget + taskBudget}</td>
                     <td></td>
 
                   </tr>
